88bdee8f05975c8c27d7273463d222cd5e7670be
kemala4680 <kema20.fathan@gmail.com>
2024-05-22 15:58:18 +0700

    fix: middle ware set headers

---
+++ our-web/src/components/chatList.jsx
@@ -0,0 +1,62 @@
+"use client";
+import { useCallback, useEffect, useState } from "react";
+import Talk from "talkjs";
+import { Session, Chatbox } from "@talkjs/react";
+
+export default function Chat({ user }) {
+  console.log(user);
+  const [dataMe, setDataMe] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch(
+          process.env.NEXT_PUBLIC_BASE_URL + `/api/user/` + userId,
+          {
+            method: "GET",
+            cache: "no-store",
+            headers: {
+              "Content-type": "application/json",
+            },
+          }
+        );
+        const data = await res.json();
+
+        setDataMe(data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  const syncUser = useCallback(
+    () =>
+      new Talk.User({
+        id: dataMe._id,
+        name: dataMe.name,
+        email: dataMe.identifier,
+        photoUrl: dataMe.imgUrl,
+        welcomeMessage: "Hi!",
+      }),
+    []
+  );
+
+  const syncConversation = useCallback((session) => {
+    // JavaScript SDK code here
+    const conversation = session.getOrCreateConversation("new_conversation");
+
+    const other = new Talk.User({
+      id: "frank",
+      name: "Frank",
+      email: "frank@example.com",
+      photoUrl: "https://talkjs.com/new-web/avatar-8.jpg",
+      welcomeMessage: "Hey, how can I help?",
+    });
+    conversation.setParticipant(session.me);
+    conversation.setParticipant(other);
+
+    return conversation;
+  }, []);
+
+  return <Session appId="t6cWkLWk" syncUser={syncUser}></Session>;
+}
